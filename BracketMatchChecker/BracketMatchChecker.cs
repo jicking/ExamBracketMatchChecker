37a1cebedde2633cc732e98514b98338646c6b4e
jicking <7297029+jicking@users.noreply.github.com>
2023-01-25 11:26:54 +0800

    set test cases

---
+++ BracketMatchChecker/BracketMatchChecker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketMatchChecker;
+public class BracketMatchChecker
+{
+    public static bool Check(string input)
+    {
+        return false;
+    }
+}
---
+++ BracketMatchCheckerTests/BracketMatchCheckerTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using BracketMatchChecker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketMatchChecker.Tests;
+public class BracketMatchCheckerTests
+{
+    [Theory]
+    [InlineData("{}", true)]
+    [InlineData("", true)]
+    [InlineData("{abc..xyz}", true)]
+    [InlineData("}{", false)]
+    [InlineData("{{}", false)]
+    public void TestIsBracketMatched(string input, bool isExpectedMatched)
+    {
+        var res = BracketMatchChecker.Check(input);
+        Assert.Equal(isExpectedMatched, res);
+    }
+}
