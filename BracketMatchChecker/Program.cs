fb2b3c53d0bff67f21c5425da7f9f43702dc515b
jicking <7297029+jicking@users.noreply.github.com>
2023-01-25 11:59:42 +0800

    implement console for live testing

--- BracketMatchChecker/Program.cs
+++ BracketMatchChecker/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace BracketMatchChecker
+﻿namespace BracketMatchChecker;
+
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        while (true) {
+            Console.WriteLine("Enter string to check whether or not characters in the string have matching brackets '{}'.");
+            string input = Console.ReadLine();
+            bool isBracketMatched = BracketMatchChecker.Check(input);
+            string hasBracketMessage = input.Contains("{") ? "" : " (no brackets in the string will return True)";
+
+            Console.WriteLine(isBracketMatched ? $"Matched {hasBracketMessage}" : "Not Matched");
+            Console.ReadLine();
+            Console.Clear();
         }
     }
+
+
 }
